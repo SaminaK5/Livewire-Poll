b161b6d7dfbc4c542737a80c714de1c1721b2040
Samina Khan <samina.k.insnapsys@gmail.com>
2023-10-26 17:21:57 +0530

    change option Remove

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,6 +1,6 @@
 <div>
-   <form wire:submit.prevent="create_Poll">
-        <label >Title</label>
+    <form wire:submit.prevent="create_Poll">
+        <label>Title</label>
 
         <input type="text" wire:model.live="title" />
         {{-- Current Title : {{ $title }} --}}
@@ -11,20 +11,20 @@
             <button class="btn" wire:click.prevent="addOption">Add Option</button>
         </div>
         <div>
-            @foreach ($options as $index => $option )
-            <div>
-               <label>Option {{ $index + 1 }}</label>
-            </div>
+            @foreach ($options as $index => $option)
+                <div>
+                    <label>Option {{ $index + 1 }}</label>
+                </div>
 
-            <div class="flex gap-2">
-                <input type="text" wire:model.live = "options.{{ $index }}">
-                <button class="btn" wire:click.prevent="remove({{ $index }})">Remove</button>
-            </div>
-            @error("options.{$index}")
+                <div class="flex gap-2">
+                    <input type="text" wire:model.live = "options.{{ $index }}">
+                    <button class="btn" wire:click.prevent="remove({{ $index }})">Remove Option</button>
+                </div>
+                @error("options.{$index}")
                     <div class="text-red-500">{{ $message }}</div>
-            @enderror
-        @endforeach
+                @enderror
+            @endforeach
         </div>
         <button class="btn" type="submit">Create Poll</button>
-   </form>
+    </form>
 </div>
