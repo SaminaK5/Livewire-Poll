585e956e296f57070b6f29b505e9253a4534b464
Samina Khan <samina.k.insnapsys@gmail.com>
2023-11-02 10:53:19 +0530

    added : after text

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -26,7 +26,7 @@
             @endforeach
         </div>
 
-        <button class="btn" type="submit">Create Poll </button>
+        <button class="btn" type="submit">Create Poll : </button>
 
     </form>
 </div>
